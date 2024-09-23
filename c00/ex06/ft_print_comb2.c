5a4bb133bd241093c97668f26baaa3c53e2942ee
Lorena Pineda <94703348+karenlorenapineda@users.noreply.github.com>
2024-09-23 20:29:02 +0200

    Add files via upload

---
+++ c00/ex06/ft_print_comb2.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpineda- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/12 20:35:51 by kpineda-          #+#    #+#             */
+/*   Updated: 2024/08/17 15:38:39 by kpineda-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_comb(int a, int b)
+{
+	ft_putchar('0' + a / 10);
+	ft_putchar('0' + a % 10);
+	ft_putchar(' ');
+	ft_putchar('0' + b / 10);
+	ft_putchar('0' + b % 10);
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	while (a <= 99)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_print_comb(a, b);
+			b++;
+			if (a != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+		a++;
+	}
+}
+/*
+int	main(void)
+{
+	ft_print_comb2();
+}*/
