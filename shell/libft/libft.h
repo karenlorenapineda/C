f21388bf39b1f0a374e88f93d128c60c013f361e
Lorena Pineda <94703348+karenlorenapineda@users.noreply.github.com>
2024-10-02 21:35:55 +0200

    Update libft.h

--- shell/libft/libft.h
+++ shell/libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/24 17:11:56 by marvin            #+#    #+#             */
-/*   Updated: 2024/09/27 00:58:42 by marvin           ###   ########.fr       */
+/*   Updated: 2024/10/02 21:35:34 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,19 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 
-
+typedef struct s_list
+{
+	void	*content;
+	struct s_list	*next;
+}					t_list;
+t_list	*ft_lstnew(void const *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 #endif
